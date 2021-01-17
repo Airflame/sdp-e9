a7588b24b846a97a29a3a181718c82e32e2bce1c
Wojciech Radzimowski <wojciech.radzimowski.99@gmail.com>
2021-01-17 17:48:39 +0100

    Checking for integer overflow

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -7,8 +7,15 @@ public class Triangle {
     public TriangleType getType() {
         if (a <= 0 || b <= 0 || c <= 0)
             throw new IllegalArgumentException("At least one number is nonpositive");
+        double da = (double)a / 2147483647;
+        double db = (double)b / 2147483647;
+        double dc = (double)c / 2147483647;
+        if (da + db > 1 || db + dc > 1 || dc + da > 1)
+            throw new IllegalArgumentException("Two edges combined exceed max integer value");
         if (a + b <= c || b + c <= a || c + a <= b)
             throw new IllegalArgumentException("Two edges combined are not longer than the third one");
+        if (a <= 0 || b <= 0 || c <= 0)
+            throw new IllegalArgumentException("At least one number is nonpositive");
         if (a == b && b == c)
             return TriangleType.EQUILATERAL;
         if (a == b || b == c || c == a)
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -13,7 +13,7 @@ public class TriangleTest {
     }
 
     @Test
-    void triangleGetType_nonpositiveInputAllEqualInput_throwException() {
+    void triangleGetType_nonpositiveAllEqualInput_throwException() {
         Triangle triangle = new Triangle(-2, -2, -2);
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, triangle::getType);
         String expectedMessage = "At least one number is nonpositive";
@@ -49,6 +49,15 @@ public class TriangleTest {
     }
 
     @Test
+    void triangleGetType_minIntNonpositiveAllEqualInput_throwException() {
+        Triangle triangle = new Triangle(-2147483648, -2147483648, -2147483648);
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, triangle::getType);
+        String expectedMessage = "At least one number is nonpositive";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
     void triangleGetType_firstAndSecondEdgeCombinedShorterThanThirdInput_throwException() {
         Triangle triangle = new Triangle(1, 1, 3);
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, triangle::getType);
@@ -76,7 +85,7 @@ public class TriangleTest {
     }
 
     @Test
-    void triangleGetType_firstAndSecondEdgeCombinedEqualsThirdInput_throwException() {
+    void triangleGetType_firstAndSecondEdgeCombinedEqualThirdInput_throwException() {
         Triangle triangle = new Triangle(2, 2, 4);
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, triangle::getType);
         String expectedMessage = "Two edges combined are not longer than the third one";
@@ -85,7 +94,7 @@ public class TriangleTest {
     }
 
     @Test
-    void triangleGetType_secondAndThirdEdgeCombinedEqualsFirstInput_throwException() {
+    void triangleGetType_secondAndThirdEdgeCombinedEqualFirstInput_throwException() {
         Triangle triangle = new Triangle(10, 5, 5);
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, triangle::getType);
         String expectedMessage = "Two edges combined are not longer than the third one";
@@ -94,7 +103,7 @@ public class TriangleTest {
     }
 
     @Test
-    void triangleGetType_thirdAndFirstEdgeCombinedEqualsSecondInput_throwException() {
+    void triangleGetType_thirdAndFirstEdgeCombinedEqualSecondInput_throwException() {
         Triangle triangle = new Triangle(6, 12, 6);
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, triangle::getType);
         String expectedMessage = "Two edges combined are not longer than the third one";
@@ -103,8 +112,83 @@ public class TriangleTest {
     }
 
     @Test
-    void triangleGetType_allEdgesEqualInput_returnEquilateral() {
+    void triangleGetType_maxIntAllEdgesInput_throwException() {
+        Triangle triangle = new Triangle(2147483647, 2147483647, 2147483647);
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, triangle::getType);
+        String expectedMessage = "Two edges combined exceed max integer value";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void triangleGetType_maxIntTwoEdgesInput_throwException() {
+        Triangle triangle = new Triangle(2147483647, 2147483647, 10);
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, triangle::getType);
+        String expectedMessage = "Two edges combined exceed max integer value";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void triangleGetType_maxIntOneEdgeInput_throwException() {
+        Triangle triangle = new Triangle(7, 7, 2147483647);
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, triangle::getType);
+        String expectedMessage = "Two edges combined exceed max integer value";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void triangleGetType_maxIntFirstAndSecondCombinedInput_throwException() {
+        Triangle triangle = new Triangle(2000000000, 2000000000, 4);
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, triangle::getType);
+        String expectedMessage = "Two edges combined exceed max integer value";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void triangleGetType_maxIntSecondAndThirdCombinedInput_throwException() {
+        Triangle triangle = new Triangle(2, 2100000000, 2100000000);
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, triangle::getType);
+        String expectedMessage = "Two edges combined exceed max integer value";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void triangleGetType_maxIntFirstAndThirdCombinedInput_throwException() {
+        Triangle triangle = new Triangle(1900000000, 3, 2100000000);
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, triangle::getType);
+        String expectedMessage = "Two edges combined exceed max integer value";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void triangleGetType_maxIntExceededByOneTwoEdgesEqualInput_returnIsosceles() {
+        Triangle triangle = new Triangle(1073741824, 1073741824, 222);
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, triangle::getType);
+        String expectedMessage = "Two edges combined exceed max integer value";
+        String actualMessage = exception.getMessage();
+        Assertions.assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void triangleGetType_allEdgesEqualTwoInput_returnEquilateral() {
         Triangle triangle = new Triangle(2, 2, 2);
+        Assertions.assertEquals(triangle.getType(), TriangleType.EQUILATERAL);
+    }
+
+    @Test
+    void triangleGetType_allEdgesEqualMaxIntCombinedInput_returnEquilateral() {
+        Triangle triangle = new Triangle(715827882, 715827882, 715827882);
+        Assertions.assertEquals(triangle.getType(), TriangleType.EQUILATERAL);
+    }
+
+    @Test
+    void triangleGetType_allEdgesEqualOneInput_returnEquilateral() {
+        Triangle triangle = new Triangle(1, 1, 1);
         Assertions.assertEquals(triangle.getType(), TriangleType.EQUILATERAL);
     }
 
@@ -127,6 +211,12 @@ public class TriangleTest {
     }
 
     @Test
+    void triangleGetType_twoEdgesEqualMaxIntCombinedInput_returnIsosceles() {
+        Triangle triangle = new Triangle(1073741823, 1073741823, 222);
+        Assertions.assertEquals(triangle.getType(), TriangleType.ISOSCELES);
+    }
+
+    @Test
     void triangleGetType_allEdgesDifferentByOneInput_returnScalene() {
         Triangle triangle = new Triangle(2, 3, 4);
         Assertions.assertEquals(triangle.getType(), TriangleType.SCALENE);
@@ -135,6 +225,18 @@ public class TriangleTest {
     @Test
     void triangleGetType_allEdgesDifferentByTwoInput_returnScalene() {
         Triangle triangle = new Triangle(6, 8, 10);
+        Assertions.assertEquals(triangle.getType(), TriangleType.SCALENE);
+    }
+
+    @Test
+    void triangleGetType_allEdgesDifferentByThreeInput_returnScalene() {
+        Triangle triangle = new Triangle(11, 14, 17);
+        Assertions.assertEquals(triangle.getType(), TriangleType.SCALENE);
+    }
+
+    @Test
+    void triangleGetType_allEdgesDifferentMaxIntCombinedInput_returnScalene() {
+        Triangle triangle = new Triangle(715827883, 715827884, 715827880);
         Assertions.assertEquals(triangle.getType(), TriangleType.SCALENE);
     }
 }
